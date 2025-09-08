4b673a8883ffa80916edce3dbff188a0c8c7ca95
Bielteknik <kenan.bielteknik@gmail.com>
2025-09-08 18:23:37 +0300

    Bu ne yav 3896 dosya nedir ya

--- Nexus__Old/Nexus/urls.py
+++
@@ -1,9 +0,0 @@
-# Nexus/urls.py
-
-from django.contrib import admin
-from django.urls import path, include # include'u ekledik
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('core.urls')), # core uygulamasının URL'lerini dahil ettik
-]
